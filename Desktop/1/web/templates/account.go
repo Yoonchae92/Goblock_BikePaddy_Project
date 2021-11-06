4f4360ceb571ef5fcd486f675f9ddff75be90abc
[Github <[Github>
2021-11-06 21:44:50 +0900

    aaa

---
+++ Desktop/1/web/templates/account.go
@@ -0,0 +1,23 @@
+<<<<<<< HEAD
+// package main
+
+// const (
+// 	//추가
+// 	user     = "root"
+// 	password = "1234"
+// 	//port     = "3307"
+// 	database = "user"
+// 	host     = "127.0.0.1"
+// )
+=======
+// package main
+
+// const (
+// 	//추가
+// 	user     = "root"
+// 	password = "1234"
+// 	//port     = "3307"
+// 	database = "user"
+// 	host     = "127.0.0.1"
+// )
+>>>>>>> 44d4b44 (22222222222222222)
---
+++ Desktop/1/web/templates/session.go
@@ -0,0 +1,121 @@
+<<<<<<< HEAD
+// package main
+
+// import (
+// 	"fmt"
+// 	"log"
+// 	"net/http"
+
+// 	"github.com/google/uuid"
+// )
+
+// func getUser(w http.ResponseWriter, req *http.Request) User {
+// 	fmt.Println("getUser()")
+// 	// get cookie
+// 	c, err := req.Cookie("session")
+// 	if err != nil {
+// 		sID := uuid.New()
+// 		c = &http.Cookie{
+// 			Name:  "session",
+// 			Value: sID.String(),
+// 		}
+// 	}
+// 	c.MaxAge = sessionLength
+// 	http.SetCookie(w, c)
+
+// 	// if the user exists already, get user
+// 	var u User
+
+// 	un, err := ReadSession(db, c.Value)
+// 	if err != nil {
+// 		log.Fatal(err)
+// 	}
+// 	UpdateCurrentTime(db, un)
+// 	u, _ = ReadUserById(db, un)
+// 	return u
+// }
+
+// func alreadyLoggedIn(w http.ResponseWriter, req *http.Request) bool {
+// 	fmt.Println("alreadyLoggedIn()")
+// 	c, err := req.Cookie("session")
+// 	if err != nil {
+// 		return false
+// 	}
+
+// 	un, err := ReadSession(db, c.Value)
+// 	if err != nil {
+// 		return false
+// 	}
+
+// 	UpdateCurrentTime(db, un)
+
+// 	_, err = ReadUserById(db, un)
+// 	if err != nil {
+// 		return false
+// 	}
+
+// 	c.MaxAge = sessionLength
+// 	http.SetCookie(w, c)
+// 	return true
+// }
+=======
+// package main
+
+// import (
+// 	"fmt"
+// 	"log"
+// 	"net/http"
+
+// 	"github.com/google/uuid"
+// )
+
+// func getUser(w http.ResponseWriter, req *http.Request) User {
+// 	fmt.Println("getUser()")
+// 	// get cookie
+// 	c, err := req.Cookie("session")
+// 	if err != nil {
+// 		sID := uuid.New()
+// 		c = &http.Cookie{
+// 			Name:  "session",
+// 			Value: sID.String(),
+// 		}
+// 	}
+// 	c.MaxAge = sessionLength
+// 	http.SetCookie(w, c)
+
+// 	// if the user exists already, get user
+// 	var u User
+
+// 	un, err := ReadSession(db, c.Value)
+// 	if err != nil {
+// 		log.Fatal(err)
+// 	}
+// 	UpdateCurrentTime(db, un)
+// 	u, _ = ReadUserById(db, un)
+// 	return u
+// }
+
+// func alreadyLoggedIn(w http.ResponseWriter, req *http.Request) bool {
+// 	fmt.Println("alreadyLoggedIn()")
+// 	c, err := req.Cookie("session")
+// 	if err != nil {
+// 		return false
+// 	}
+
+// 	un, err := ReadSession(db, c.Value)
+// 	if err != nil {
+// 		return false
+// 	}
+
+// 	UpdateCurrentTime(db, un)
+
+// 	_, err = ReadUserById(db, un)
+// 	if err != nil {
+// 		return false
+// 	}
+
+// 	c.MaxAge = sessionLength
+// 	http.SetCookie(w, c)
+// 	return true
+// }
+>>>>>>> 44d4b44 (22222222222222222)
